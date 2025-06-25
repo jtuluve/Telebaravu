279774091a920dc848ea66aca9324b8d6e6ca789
Jnanesh <74163310+jtuluve@users.noreply.github.com>
2025-06-25 22:49:10 +0530

    Add broadcast in api

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,47 @@ async function startServer() {
     app.use(
       webhookCallback(bot, "express", "throw", 90000, process.env.WEBHOOK_TOKEN)
     );
+    app.post("/broadcast", async (req, res) => {
+      try {
+        const { code, message } = req.body;
+    
+        // Validate the secret code
+        if (code !== process.env.BROADCAST_SECRET) {
+          return res.status(401).json({ success: false, message: "Invalid code." });
+        }
+        await broadcastMessage(message)
+    
+      } catch (error) {
+        console.error("Error handling broadcast:", error);
+        res.status(500).json({ success: false, message: "Internal server error." });
+      }
+    });
 
     await connectDB();
   } else {
     // Use Long Polling for development
     connectDB().then(() => connectAgenda().then(() => bot.start()));
+  }
+}
+
+async function broadcastMessage(message: string) {
+  const users = await dbget();
+
+  for (let i = 0; i < users.length; i++) {
+    let userId = users[i].userid;
+    try {
+      await bot.api.sendMessage(userId, message);
+    } catch (error) {
+      // Check if it's a rate limit error
+      if (error.code === 429) {
+        const waitTime = error.parameters.retry_after;
+        await new Promise((resolve) => setTimeout(resolve, waitTime * 1001));
+        i--;
+      } else {
+        console.log(`Failed to send message to ${userId}:`, error);
+        await dbdelete(userId);
+      }
+    }
   }
 }
 startServer();
