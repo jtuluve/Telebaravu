fb6556dfac38ab4d927308b57d8322432e04613b
Jnanesh <74163310+jtuluve@users.noreply.github.com>
2025-09-04 09:19:42 +0530

    Improved inc and added method for total count

--- src/dbfunc.ts
+++ src/dbfunc.ts
@@ -20,7 +20,7 @@ const users = mongoose.model<User>("user", userSchema);
 
 export async function connectDB() {
   try {
-    if (mongoose.connection.readyState === 1) throw "Already connected";
+    if (mongoose.connection.readyState === 1) return;
     await mongoose.connect(process.env.MONGO_URL!);
     console.log("Connected to db.");
   } catch (error) {
@@ -83,11 +83,25 @@ export async function dbdelete(userid: number) {
 
 export async function incrementCount(userid: number) {
   try {
-    const user = await dbget(userid) as User;
-    if (!user) return null;
-    return await dbupdate(userid, { count: (user.count||0) + 1 });
+    return await users.findOneAndUpdate({ userid }, { $inc: { count: 1 } });
   } catch (err) {
     console.error(err);
     return null;
+  }
+}
+
+export async function getTotalCount(){
+  try{
+    return (await users.aggregate([
+      {
+        $group: {
+          _id: null,
+          total: { $sum: "$count" }
+        }
+      }
+    ]))[0]?.total || 0;
+  } catch (err) {
+    console.error(err);
+    return 0;
   }
 }
